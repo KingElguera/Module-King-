d834d3fd4a6343b5dcf3052519c6cc6764df9128
KingElguera <elgueraishimwe@gmail.com>
2025-10-02 13:13:51 +0000

    premiercode

---
+++ main.py
@@ -0,0 +1,30 @@
+# age = 20 
+# height = 1.80
+# name = "kingelguera"
+# is_student = True 
+# print(age,height,name,is_student)
+
+
+# Exercice 2 
+# a = 50
+# b = 10
+# print(a*b)
+# print(a+b)
+# print(a-b)
+
+# a = 100
+# b = 2
+# exponentiation = a**b 
+# print(exponentiation)
+
+
+# Exercice 3
+first_name = "king elguera"
+last_name = "ishimwe" 
+full_name = first_name + last_name
+full_name = "ishimwekingelguera"
+print(full_name(split))
+
+
+
+
